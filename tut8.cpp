c6026a07073d1a3db5e53ac192258bddc5d96786
shahzaibjameel016-web <shahzaibjameel016@gmail.com>
2025-11-03 18:25:20 +0500

    Add files via upload

---
+++ tut8.cpp
@@ -0,0 +1,16 @@
+
+            //task lab #5
+            
+#include<iostream>
+using namespace std;
+int main(){
+    
+for(int i=1;i<=8;i++){
+    for(int j=1;j<=8;j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+
+return 0;
+}
